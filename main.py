f3c805623ac198de8a566736c92f3fbbfed01bae
Jurrer <jk20@op.pl>
2022-03-26 16:34:29 +0100

    got class extraction working

--- main.py
+++ main.py
@@ -57,73 +57,83 @@ def polob(listaobr, ile_k=1, listatyt=[], openCV=True, wart_dpi=100, osie=False,
             pokaz(listaobr[i], listatyt[i], osie, openCV, colmap)
     plt.show()
 
-def ekstrakcja_cech(o):
+
+def ekstrakcja_cech(obiekt_do_ekstrakcji_cech):
     # ekstrakcja cech
     # binaryzacja obrazu
-    b = cv2.inRange(o,(1,1,1),(255,255,255))
+    b = cv2.inRange(obiekt_do_ekstrakcji_cech, (1, 1, 1), (255, 255, 255))
     # etykietowanie i ekstrakcja cech
     cechy = regionprops(label(b))
     ile_obiektow = len(cechy)
-    lista_cech = ['EulerNumber','Area','BoundingBoxArea','FilledArea','Extent','EquivDiameter','Solidity']
+    lista_cech = ['EulerNumber', 'Area', 'BoundingBoxArea', 'FilledArea', 'Extent', 'EquivDiameter', 'Solidity']
     ile_cech = len(lista_cech)
-    tabela_cech = np.zeros((ile_obiektow,ile_cech+1+7)) # "1" - to jedna cecha wyliczna, "7" to momenty Hu
+    tabela_cech = np.zeros((ile_obiektow, ile_cech + 1 + 7))  # "1" - to jedna cecha wyliczna, "7" to momenty Hu
     listaob = []
-    for i in range(0,ile_obiektow):
-        yp,xp,yk,xk = cechy[i]['BoundingBox']
-        aktualny_obiekt = o[yp:yk,xp:xk,:]
-        ret,binobj = cv2.threshold(aktualny_obiekt[:,:,1],0,255,cv2.THRESH_BINARY)
-        listaob.append(binobj) #aktualny_obiekt)
+    for i in range(0, ile_obiektow):
+        yp, xp, yk, xk = cechy[i]['BoundingBox']
+        aktualny_obiekt = obiekt_do_ekstrakcji_cech[yp:yk, xp:xk, :]
+        ret, binobj = cv2.threshold(aktualny_obiekt[:, :, 1], 0, 255, cv2.THRESH_BINARY)
+        listaob.append(binobj)  # aktualny_obiekt)
         # rejestrujemy wybrane cechy wyznaczone przez regionprops
-        for j in range(0,ile_cech):
-            tabela_cech[i,j] = cechy[i][lista_cech[j]]
+        for j in range(0, ile_cech):
+            tabela_cech[i, j] = cechy[i][lista_cech[j]]
         # dodajemy momenty Hu
         hu = cv2.HuMoments(cv2.moments(binobj))
-        hulog = (1 - 2*(hu>0).astype('int'))* np.nan_to_num(np.log10(np.abs(hu)),copy=True,neginf=-99,posinf=99)
-        tabela_cech[i,ile_cech+1:ile_cech+8] = hulog.flatten()
-    tabela_cech[:,ile_cech] = tabela_cech[:,3]/tabela_cech[:,2] # cecha wyliczana
-    tabela_cech[:,0] = (tabela_cech[:,0] == 1) # korekta liczby Eulera
+        hulog = (1 - 2 * (hu > 0).astype('int')) * np.nan_to_num(np.log10(np.abs(hu)), copy=True, neginf=-99, posinf=99)
+        tabela_cech[i, ile_cech + 1:ile_cech + 8] = hulog.flatten()
+    tabela_cech[:, ile_cech] = tabela_cech[:, 3] / tabela_cech[:, 2]  # cecha wyliczana
+    tabela_cech[:, 0] = (tabela_cech[:, 0] == 1)  # korekta liczby Eulera
     return listaob, tabela_cech
 
-def ekstrakcja_klas(o):
+
+def ekstrakcja_klas(obiekt_do_ekstrakcji_klas):
     # ekstrakcja kategorii
     # binaryzacja obrazu
-    b = cv2.inRange(o,(1,1,1),(255,255,255))
+    b = cv2.inRange(obiekt_do_ekstrakcji_klas, (1, 1, 1), (255, 255, 255))
     # etykietowanie i ekstrakcja cech
     cechy = regionprops(label(b))
+    print("cechy: ", cechy)
     ile_obiektow = len(cechy)
+    print("ile obiektów: ", ile_obiektow)
     # wyszukiwanie kolorów
-    kolory = np.unique(o.reshape(-1, o.shape[2]), axis=0) # kolory w obrazie
+    kolory = np.unique(obiekt_do_ekstrakcji_klas.reshape(-1, obiekt_do_ekstrakcji_klas.shape[2]), axis=0)  # kolory w obrazie
     # według kolorów przypiszemy klasy obiektom
-    kolory = kolory[1:7,:] # usuwa kolor tła
+    kolory = kolory[1:7, :]  # usuwa kolor tła
     ile_kategorii = len(kolory)
-    kategorie = np.zeros((ile_obiektow,1)).astype('int')
+    print("Kategorie: ", ile_kategorii)
+    kategorie = np.zeros((ile_obiektow, 1)).astype('int')
     listaob = []
-    for i in range(0,ile_obiektow):
-        x,y = cechy[i]['Coordinates'][1] # wsp. jednego z punktów obiektu - do próbkowania koloru
-        for k in range(0,ile_kategorii):
-            if list(o[x,y,:]) == list(kolory[k]):
-                break;
+    for i in range(0, ile_obiektow):
+        x, y = cechy[i]['Coordinates'][1]  # wsp. jednego z punktów obiektu - do próbkowania koloru
+        for k in range(0, ile_kategorii):
+            if list(obiekt_do_ekstrakcji_klas[x, y, :]) == list(kolory[k]):
+                break
         kategorie[i] = k
     return kategorie
 
 
 if __name__ == '__main__':
     path_to_vid = './model/PA_1.avi'
-    path_to_pic = './model/PA_1_ref.png'
+    path_to_pic1 = './model/PA_1_ref.png'
+    path_to_pic2 = './model/obiekty.png'
     video = cv2.VideoCapture(path_to_vid)
     destroy_vid(video)
 
     # wczytanie obrazu
-    o = cv2.imread(path_to_pic)
-    b = cv2.inRange(o, (1, 1, 1), (255, 255, 255))
+    o = cv2.imread(path_to_pic1)
+    p = cv2.imread(path_to_pic2)
+    aktualnie_badany = o
+
+    b = cv2.inRange(aktualnie_badany, (1, 1, 1), (255, 255, 255))
     # etykietowanie
     etykiety = label(b)
-    polob([o, b, etykiety], 3)
+    polob([aktualnie_badany, b, etykiety], 2)
     # wyznaczanie cech
     cechy = regionprops(etykiety)
 
-    lo, tc = ekstrakcja_cech(o)
-    ka = ekstrakcja_klas(o)
-    print(ka)
+    lo, tc = ekstrakcja_cech(aktualnie_badany)
+    ka = ekstrakcja_klas(aktualnie_badany)
+    print(tc[1])
+    print(ka[1])
 
-    polob(lo,10,colmap='winter')
+    polob(lo, 10, colmap='winter')
